f9fc9c0a3fd792c34181219f45efde6d6efd5f04
gsousa12 <wgsousa12@gmail.com>
2026-07-24 15:44:20 -0300

    feat: init

---
+++ index.ts
@@ -0,0 +1,14 @@
+import * as net from "net";
+
+function newConn(socket: net.Socket): void {
+  console.log("New Conection:", socket.remoteAddress, socket.remotePort);
+}
+
+let server = net.createServer();
+
+server.on("connection", newConn);
+
+server.listen({
+  host: "127.0.0.1",
+  port: 1234,
+});
